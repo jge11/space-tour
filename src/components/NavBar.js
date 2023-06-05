56b8702c52e5e86073f9467358c461aac2815316
Javier-Garcia-Estrada <javiergarciaestrada7@gmail.com>
2023-06-05 18:22:32 -0400

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,25 @@
 import React from "react";
 import logo from "../assets/shared/logo.svg";
+import { Link } from "react-router-dom";
+import Destination from "./Destination";
 
 const NavBar = () => {
   return (
     <div className="navBar">
       <div className="leftSide">
-        <img src={logo} />
+        <Link to="/space-tour">
+          <img src={logo} />
+        </Link>
       </div>
-      <div className="rightSide"></div>
+      <div className="rightSide">
+        <Link to="/space-tour">Home</Link>
+        <br />
+        <Link to={Destination}>Destination</Link>
+        <br />
+        <Link to="/space-tour/space-tour/Crew">Crew</Link>
+        <br />
+        <Link to="/space-tour/Techonology">Technology</Link>
+      </div>
     </div>
   );
 };
