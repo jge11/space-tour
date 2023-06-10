0b444f049adca5397451b7cadc94b8fd0abfdac0
Javier-Garcia-Estrada <javiergarciaestrada7@gmail.com>
2023-06-09 21:54:25 -0400

    fixed navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import Earth from "../assets/home/background-home-desktop.jpg";
 const Home = () => {
   return (
     <div className="home">
-      <img src={Earth} alt="" />
+      <img className="earth" src={Earth} alt="" />
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import logo from "../assets/shared/logo.svg";
-import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
 import "../styles/styles.css";
 
@@ -12,19 +11,15 @@ const NavBar = () => {
           <img src={logo} />
         </Link>
       </div>
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <div className="rightSide">
-            <Link to="/space-tour">00 Home</Link>
-            <br />
-            <Link to="/space-tour/Destination">01 Destination</Link>
-            <br />
-            <Link to="/space-tour/Crew">03 Crew</Link>
-            <br />
-            <Link to="/space-tour/Techonology">04 Technology</Link>
-          </div>
-        </Grid>
-      </Grid>
+      <div className="rightSide">
+        <Link to="/space-tour">00 Home</Link>
+        <br />
+        <Link to="/space-tour/Destination">01 Destination</Link>
+        <br />
+        <Link to="/space-tour/Crew">03 Crew</Link>
+        <br />
+        <Link to="/space-tour/Techonology">04 Technology</Link>
+      </div>
     </div>
   );
 };
